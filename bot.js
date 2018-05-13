69ad1b2f1d9dc5aa8cb5460fc96226f23a05c601
Mwoodworth1 <39204338+Mwoodworth1@users.noreply.github.com>
2018-05-12 20:38:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ let request = axios.create({
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity(`Fatality.net | !help`);
+    bot.user.setActivity(`FatalityMC.net | !help`);
 });
 
 
